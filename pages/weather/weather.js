37bb9e16608b56e6c7da9232fe148f2eb55cc4fb
杜塞尔多夫 <2511881919@qq.com>
2018-03-28 16:55:11 +0800

    add some file

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -1,3 +1,6 @@
+var util=require('../../utils/util.js');
+
+
 Page({
     data:{
        weather:{
@@ -56,7 +59,75 @@ Page({
            ]
        },
         today:'2018-03-27',
-        city:'北京',
-        inputCity:''
+        city:'北京', //城市名称
+        inputCity:''   //输入查询的城市名称
+    },
+    onLoad:function (options) {
+        this.setData({
+            today:util.formatTime(new Date()).split(' ')[0]
+        });
+        var self=this;
+        wx.getLocation({
+
+            type:'wgs84',
+            success:function (res) {
+                wx.request({
+                    url:'http://api.map.baidu.com/geocoder/v2/'+'ak=ASAT5N3tnHIa4APW0SNPeXN5&location='+res.latitude+','+res.longitude+'&output=json&pois=0',
+                    data: {},
+                    header:{
+                        'Content-Type':'application/json'
+                    },
+                    success:function (res) {
+                        var city=res.data.result.addressComponent.city.replace('市',''); //城市名称
+                        self.searchWeather(city);
+                    }
+
+
+
+                })
+
+
+            }
+
+        })
+    },
+    searchWeather:function (cityName) {
+        var self=this;
+        wx.request({
+            url:'http://wthrcdn.etouch.cn/weather_mini?city='+cityName,
+            data:{},
+            header:{
+                'Content-Type':'application/json'
+            },
+            success:function (res) {
+                //显示错误信息
+                if(res.data.status==1002){
+                    wx.showModal({
+                        title:'提示',
+                        content:'输入的城市名称有误，请重新输入！',
+                        showCancel:false,
+                        success:function (res) {
+                            self.setData({
+                                inputCity:''
+                            })
+                        }
+
+                    })
+                }else{
+                    var weather=res.data.data;           //获取天气数据
+                    for(var i=0;i<weather.forecast.length;i++){
+                        var d=weather.forecast[i].date;
+                        //处理日期信息，添加空格
+                        weather.forecast[i].date=''+d.replace('星期',' 星期');
+                    }
+                    self.setData({
+                        city:cityName,   //更新显示城市名称
+                        weather:weather,   //更新天气信息
+                        inputCity:''       //清空查询输入框
+                    })
+                }
+            }
+        })
     }
 })
+
